2eaa43c15bd9bbdea0b44681834936815f9f45b6
franko88bit <franks188@icloud.com>
2023-04-18 19:41:31 -0400

    added generate PW function and program is working

--- Develop/script.js
+++ Develop/script.js
@@ -3,15 +3,19 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+console.log(generateBtn);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+}
+// This is the algo to generate the password according to the user
+function generatePassword() { 
+  var password_to_return = "example output password";
+  return password_to_return;
 }
 
 // Add event listener to generate button
+console.log("hi there");
 generateBtn.addEventListener("click", writePassword);
