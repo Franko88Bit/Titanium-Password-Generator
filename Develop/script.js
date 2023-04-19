762ab938cdd393d6cbd8d2c3230794633dbcc7f6
franko88bit <franks188@icloud.com>
2023-04-18 21:46:24 -0400

    added rest of requirements and came up with simple algo to generate password based on user inputs

--- Develop/script.js
+++ Develop/script.js
@@ -10,10 +10,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-// This is the algo to generate the password according to the user
+// Steps to generate the password based on user's selections
 function generatePassword() { 
   var password_to_return = "";
-  // first requirement is to ask user how many characters
+  // first requirement is to ask user how many characters, answer is returned in string so need to convert to number
   var num_of_characters = Number(prompt("How many characters do you want the password to be?"));
   console.log(num_of_characters);
   // bad case, want user to retry
@@ -21,25 +21,69 @@ function generatePassword() {
     alert("please key in a number of at least 8 characters and no more than 128 characters and try again");
     generatePassword();
   }
-
-  // second requirement is to check if user wants to include special characters in password
-  var if_include_special_character = confirm("Do you want to include special characters?");
-  if (if_include_special_character == false) {
-    if_include_special_character = false;
-    //console.log(if_include_special_character);
-    //console.log("false special char");
+  // good case for number entered
+  else {
+    // second requirement is to check if user wants to include special characters in password
+    var if_include_special_character = confirm("Do you want to include special characters?");
+    if (if_include_special_character == false) {
+      if_include_special_character = false;
+    }
+    else {
+      if_include_special_character = true;
+    }
+    // third requirement is to check if user wants to include numerics in password
+    var if_include_number = confirm("Do you want to include numbers?");
+    if (if_include_number == false) {
+      if_include_number = false;
+    }
+    else {
+      if_include_number = true;
+    }
+    // forth requirement is to check if user wants to include upper case letters
+    var if_include_upper_case = confirm("Do you want to include uppercase letters?");
+    if (if_include_upper_case == false) {
+      if_include_upper_case = false;
+    }
+    else {
+      if_include_upper_case = true;
+    }
+    // fifth final requirement is to check if user wants to include lower case letters
+    var if_include_lower_case = confirm("Do you want to include lowercase letters?");
+    if (if_include_lower_case == false) {
+      if_include_lower_case = false;
+    }
+    else {
+      if_include_lower_case = true;
+    }
   }
-  if_include_special_character = true;
-  
-  // third requirement is to check if user wants to include numerics aka numbers in password
-  var if_include_number = confirm("Do you want to include numbers?");
-  if (if_include_number == false) {
-    if_include_number = false;
-  } 
-  if_include_number = true;
-
+  //password_to_return = generatePasswordWithUserSelections();
   return password_to_return;
 }
 
+// specific, simple algorithm to generate password that satisfies user's selection, based on the 5 inputs: 1 number, 4 booleans
+/*function generatePasswordWithUserSelections(num_of_characters, if_include_special_character, if_include_number, if_include_upper_case, if_include_lower_case) {
+  // came up with a simple way that satisfies the acceptance criteria
+  for (let i = 0; i < num_of_characters; ++i) {
+    // first generate an arbitrary character for each index, using 'f'
+    let f_char = "f";
+    if () {
+
+    }
+    else if () {
+
+    }
+    else if () {
+
+    }
+    else if () {
+
+    }
+  }
+}*/
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// password needs to be length 4
+// _ _ _ _
+// & F f 1
