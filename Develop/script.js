96fe7824a7b09358b68c04dd27f9d46c602ea7f2
franko88bit <franks188@icloud.com>
2023-04-18 20:22:49 -0400

    asked user if they want special characters and numbers in password

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,10 +12,34 @@ function writePassword() {
 }
 // This is the algo to generate the password according to the user
 function generatePassword() { 
-  var password_to_return = "example output password";
+  var password_to_return = "";
+  // first requirement is to ask user how many characters
+  var num_of_characters = Number(prompt("How many characters do you want the password to be?"));
+  console.log(num_of_characters);
+  // bad case, want user to retry
+  if ((num_of_characters <= 7) || (num_of_characters >= 129)) {
+    alert("please key in a number of at least 8 characters and no more than 128 characters and try again");
+    generatePassword();
+  }
+
+  // second requirement is to check if user wants to include special characters in password
+  var if_include_special_character = confirm("Do you want to include special characters?");
+  if (if_include_special_character == false) {
+    if_include_special_character = false;
+    //console.log(if_include_special_character);
+    //console.log("false special char");
+  }
+  if_include_special_character = true;
+  
+  // third requirement is to check if user wants to include numerics aka numbers in password
+  var if_include_number = confirm("Do you want to include numbers?");
+  if (if_include_number == false) {
+    if_include_number = false;
+  } 
+  if_include_number = true;
+
   return password_to_return;
 }
 
 // Add event listener to generate button
-console.log("hi there");
 generateBtn.addEventListener("click", writePassword);
